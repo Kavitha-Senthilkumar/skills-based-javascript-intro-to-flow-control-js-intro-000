84db43f3e197d69451f060cc0b560e2ab0f233c1
Kavitha Senthilkumar <kvithaa@gmail.com>
2020-10-08 18:59:51 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -14,7 +14,14 @@ return('You are not a teenager');
 }
 
 function ageChecker(age) {
-
+if (age>=13 && age<=19)
+{
+  return('You are a teenager!');
+}
+elseif(age<12){
+  return('You are a kid')
+}
+return('You are a grownup')
 }
 
 function ternaryTeenager(age) {
